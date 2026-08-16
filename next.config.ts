57f5e218126a9373e182b531ff35c2747d972221
Chinmayi P H <phchinmayi93@gmail.com>
2026-08-16 20:47:13 +0530

    adding photos

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["172.16.4.106:3000", "localhost:3000", "172.16.4.106"],
-  
+
   typescript: {
     ignoreBuildErrors: true,
   },
 
+  // Supported in Next.js 14+ / 15 / 16
+  serverActions: {
+    bodySizeLimit: "100mb",
+  },
+
   experimental: {
     serverActions: {
-      bodySizeLimit: "10mb",
+      bodySizeLimit: "100mb",
     },
   },
 
-  // Bypass Next.js 16 type error for ESLint
+  // Bypass Next.js type error for ESLint
   ...({
     eslint: {
       ignoreDuringBuilds: true,
